32f5ad08629bfa99dbe681147731c8f817e06b16
Mahdi Delzendeh <m.delzendeh72@gmail.com>
2022-12-23 17:59:34 +0330

    add interfaces

---
+++ src/CSharp/EasyMicroservice.Serialization/Interfaces/IBinarySerialization.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace EasyMicroservice.Serialization.Interfaces
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IBinarySerialization : IBaseSerialization
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        ReadOnlySpan<byte> Serialize(object value);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="reader"></param>
+        /// <returns></returns>
+        T Deserialize<T>(ReadOnlySpan<byte> reader);
+
+    }
+
+}
