77b827a25a64cb71e659d15b6731a5cd9c4cb35b
Mahdi Delzendeh <m.delzendeh72@gmail.com>
2022-12-23 18:00:29 +0330

    add system text provider

---
+++ src/CSharp/EasyMicroservice.Serialization.System.Text.Json/Providers/SystemTextJsonBinaryProvider.cs
@@ -0,0 +1,37 @@
+﻿#if (!NET45)
+using EasyMicroservice.Serialization.Providers.SerializationProvider;
+using System;
+using System.Text.Json;
+
+namespace EasyMicroservice.Serialization.System.Text.Json.Providers
+{
+    /// <summary>
+    /// /
+    /// </summary>
+
+    public class SystemTextJsonBinaryProvider : BaseBinarySerializationProvider
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="reader"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public override T Deserialize<T>(ReadOnlySpan<byte> reader)
+        {
+            return JsonSerializer.Deserialize<T>(reader);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public override ReadOnlySpan<byte> Serialize(object value)
+        {
+            return JsonSerializer.SerializeToUtf8Bytes(value);
+        }
+    }
+}
+#endif
