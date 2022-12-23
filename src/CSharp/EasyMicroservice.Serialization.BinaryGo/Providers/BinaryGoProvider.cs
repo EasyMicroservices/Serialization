a41c8d5beae0e47aa2f9f035e85aa820934267b9
Mahdi Delzendeh <m.delzendeh72@gmail.com>
2022-12-23 17:59:49 +0330

    add binary go provider

---
+++ src/CSharp/EasyMicroservice.Serialization.BinaryGo/Providers/BinaryGoProvider.cs
@@ -0,0 +1,33 @@
+﻿using BinaryGo.Binary;
+using BinaryGo.Binary.Deserialize;
+using System;
+
+namespace EasyMicroservice.Serialization.Providers.SerializationProvider
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class BinaryGoProvider : BaseBinarySerializationProvider
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="reader"></param>
+        /// <returns></returns>
+        public override T Deserialize<T>(ReadOnlySpan<byte> reader)
+        {
+            return BinaryDeserializer.NormalInstance.Deserialize<T>(reader);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public override ReadOnlySpan<byte> Serialize(object value)
+        {
+            return BinarySerializer.NormalInstance.Serialize(value);
+        }
+    }
+}
