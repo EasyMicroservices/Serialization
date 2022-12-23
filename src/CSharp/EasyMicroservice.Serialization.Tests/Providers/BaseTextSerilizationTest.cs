34d5c5cad86e9b5c45299dc44a369408967ec228
Mahdi Delzendeh <m.delzendeh72@gmail.com>
2022-12-23 18:00:44 +0330

    add test solution

---
+++ src/CSharp/EasyMicroservice.Serialization.Tests/Providers/BaseTextSerilizationTest.cs
@@ -0,0 +1,21 @@
+﻿using EasyMicroservice.Serialization.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyMicroservice.Serialization.Tests.Providers
+{
+    public abstract class BaseTextSerilizationTest
+    {
+        public ITextSerialization _provider { get; }
+        public BaseTextSerilizationTest(ITextSerialization provider)
+        {
+            _provider = provider;
+        }
+
+        []
+        public async Task 
+       
+    }
+}
