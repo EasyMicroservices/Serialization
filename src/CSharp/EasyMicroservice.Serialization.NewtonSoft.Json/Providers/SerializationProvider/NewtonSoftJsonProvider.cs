9e2ae4e091a1cf8d218c4dc5d06cce06bcbca733
Mahdi Delzendeh <m.delzendeh72@gmail.com>
2022-12-23 18:00:04 +0330

    add newtonsoftjson provider

---
+++ src/CSharp/EasyMicroservice.Serialization.NewtonSoft.Json/Providers/SerializationProvider/NewtonSoftJsonProvider.cs
@@ -0,0 +1,33 @@
+﻿using EasyMicroservice.Serialization.Providers.SerializationProvider;
+using Newtonsoft.Json;
+using System;
+
+namespace EasyMicroservice.Serialization.NewtonSoft.Json.Providers.SerializationProvider
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class NewtonSoftJsonProvider : BaseTextSerializationProvider
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public override T Deserialize<T>(string value)
+        {
+            return JsonConvert.DeserializeObject<T>(value);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public override string Serialize(object value)
+        {
+            return JsonConvert.SerializeObject(value);
+        }
+    }
+}
