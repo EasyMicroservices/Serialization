9bbf795aa529f450d031a96d364109b4491c0bfc
Mahdi Delzendeh <m.delzendeh72@gmail.com>
2022-12-28 17:32:36 +0330

    add test for textSerilazation

---
+++ src/CSharp/EasyMicroservices.Serialization.Tests/Providers/TextSerilization/BaseTextSerilizationTest.cs
@@ -0,0 +1,75 @@
+﻿using EasyMicroservices.Serialization.Interfaces;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EasyMicroservices.Serialization.Tests.Providers.TextSerilization
+{
+    /// <summary>
+    /// base class for test
+    /// </summary>
+    public abstract class BaseTextSerilizationTest
+    {
+        /// <summary>
+        /// text serilaze provider
+        /// </summary>
+        public ITextSerialization _provider { get; }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="provider"></param>
+        public BaseTextSerilizationTest(ITextSerialization provider)
+        {
+            _provider = provider;
+        }
+
+        [Theory]
+        [InlineData("Mahdi", 30, Gender.Male)]
+        [InlineData("Maryam", 15, Gender.Female)]
+        public async Task Serilize(string name, int age, Gender gender)
+        {
+            var request = new ClassToSerialize()
+            {
+                Name = name,
+                Age = age,
+                Gender = gender
+            };
+            var result = _provider.Serialize(request);
+            Assert.Equal(result, "{\"Name\":\"" + name + "\",\"Age\":" + age + "\",\"Gender\":" + (int)gender + "}");
+        }
+        /// <summary>
+        /// میخواهیم یک متن جیسون را به کلاس تبدیل کنیم
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="age"></param>
+        [InlineData("Mahdi", 30, Gender.Male)]
+        [InlineData("Maryam", 15, Gender.Female)]
+        [Theory]
+        public async Task Deserilize(string name, int age, Gender gender)
+        {
+            var request = new ClassToSerialize()
+            {
+                Name = name,
+                Age = age,
+                Gender = gender
+            };
+            var json = _provider.Serialize(request);
+            var result = _provider.Deserialize<ClassToSerialize>(json);
+
+            Assert.True(result.Name == request.Name && result.Age == request.Age && result.Gender == request.Gender);
+        }
+
+    }
+
+    public class ClassToSerialize
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+    }
+    public enum Gender
+    {
+        None = 0,
+        Male = 1,
+        Female = 2
+    }
+}
---
+++ src/CSharp/EasyMicroservices.Serialization.Tests/Providers/TextSerilization/SystemTextJsonTest.cs
@@ -0,0 +1,13 @@
+﻿#if (!NET45)
+using EasyMicroservices.Serialization.System.Text.Json.Providers;
+
+namespace EasyMicroservices.Serialization.Tests.Providers.TextSerilization
+{
+    public class SystemTextJsonTest : BaseTextSerilizationTest
+    {
+        public SystemTextJsonTest() : base(new SystemTextJsonProvider())
+        {
+        }
+    }
+}
+#endif
