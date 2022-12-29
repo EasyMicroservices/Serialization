cbc0e557272dd778f88a0e2063b6106bf6b64275
Mohsen Yaghoubi <Mohsen-Yaghoubi@outlook.com>
2022-12-29 14:59:36 +0330

    Add Yaml serialization to project

---
+++ src/CSharp/EasyMicroservices.Serialization.Yaml/Providers/DotNetYamlProvider.cs
@@ -0,0 +1,47 @@
+﻿#if(!NET45)
+using System;
+using System.Text.Json;
+using EasyMicroservices.Serialization.Providers;
+using YamlDotNet.Core;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace EasyMicroservices.Serialization.System.Text.Json.Providers
+{
+    /// <summary>
+    ///  use YamlDotNet as text serialization provider 
+    /// </summary>
+    public class DotNetYamlProvider : BaseTextSerializationProvider
+    {
+        /// <summary>
+        /// Deserialize from string
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public override T Deserialize<T>(string value)
+        {
+            var deserializer = new DeserializerBuilder()
+            .WithNamingConvention(PascalCaseNamingConvention.Instance)
+            .Build();
+
+            return deserializer.Deserialize<T>(value);
+        }
+        /// <summary>
+        /// Serialize to string
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public override string Serialize(object value)
+        {
+            var serializer = new SerializerBuilder()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .Build();
+
+            return serializer.Serialize(value);
+        }
+    }
+}
+#endif
