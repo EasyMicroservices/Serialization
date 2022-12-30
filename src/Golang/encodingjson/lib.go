39cee04013d3eb7624dba57b4579235731b23a12
ashkan <kotksthegame@gmail.com>
2022-12-30 07:02:07 +0330

    - Add comments.

--- src/Golang/encodingjson/lib.go
+++ src/Golang/encodingjson/lib.go
@@ -2,9 +2,11 @@ package encodingjson
 
 import "encoding/json"
 
+// JsonSerializer is a string serializer which uses encoding/json package.
 type JsonSerializer struct {
 }
 
+// Serialize is implementation of serialize method from StringSerializer interface.
 func (j JsonSerializer) Serialize(value any) (string, error) {
 	res, err := json.Marshal(value)
 
@@ -14,6 +16,7 @@ func (j JsonSerializer) Serialize(value any) (string, error) {
 	return string(res), nil
 }
 
+// Deserialize is implementation of deserialize method from StringSerializer interface.
 func (j JsonSerializer) Deserialize(s string, v any) error {
 	return json.Unmarshal([]byte(s), v)
 }
