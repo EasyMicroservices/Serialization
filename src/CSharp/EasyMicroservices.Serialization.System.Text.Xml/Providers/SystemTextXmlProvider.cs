d85f1110483884bd050588cec16a84b46365b994
mohammadreza mardani <mr-mardani@yahoo.com>
2022-12-31 12:31:14 +0330

    Add Xml Serializer

---
+++ src/CSharp/EasyMicroservices.Serialization.System.Text.Xml/Providers/SystemTextXmlProvider.cs
@@ -0,0 +1,43 @@
+﻿using EasyMicroservices.Serialization.Providers;
+using System.IO;
+using System.Xml.Serialization;
+using System.Xml;
+
+namespace EasyMicroservices.Serialization.System.Text.Xml.Providers
+{
+    /// <summary>
+    ///  use mocrosoft SystemTextJson package as text serialization provider 
+    /// </summary>
+    public class SystemTextXmlProvider : BaseTextSerializationProvider
+    {
+        /// <summary>
+        /// Deserialize from string        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public override T Deserialize<T>(string value)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(T));
+            return (T)xmlSerializer.Deserialize(new StringReader(value));
+        }
+        /// <summary>
+        /// Serialize to string
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public override string Serialize(object value)
+        {
+            XmlSerializer xsSubmit = new XmlSerializer(value.GetType());
+            using (var sww = new StringWriter())
+            {
+                using (XmlTextWriter writer = new XmlTextWriter(sww) { Formatting = Formatting.Indented })
+                {
+                    xsSubmit.Serialize(writer, value);
+                    return sww.ToString();
+                }
+            }
+        }
+    }
+}
---
+++ src/CSharp/EasyMicroservices.Serialization.Tests/Providers/TextSerialization/SystemTextXmlSerializationTest.cs
@@ -0,0 +1,37 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using EasyMicroservices.Serialization.System.Text.Xml.Providers;
+using EasyMicroservices.Serialization.Tests.Providers.Models;
+using Xunit;
+
+namespace EasyMicroservices.Serialization.Tests.Providers.TextSerialization
+{
+    public class SystemTextXmlSerializationTest : BaseTextSerializationTest
+    {
+        public SystemTextXmlSerializationTest() : base(new SystemTextXmlProvider())
+        {
+        }
+
+        [InlineData("Mahdi", 30, Gender.Male, "<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ClassToSerialize xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <Name>Mahdi</Name>\r\n  <Age>30</Age>\r\n  <Gender>Male</Gender>\r\n</ClassToSerialize>")]
+        [InlineData("Maryam", 15, Gender.Female, "<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ClassToSerialize xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <Name>Maryam</Name>\r\n  <Age>15</Age>\r\n  <Gender>Female</Gender>\r\n</ClassToSerialize>")]
+        [InlineData("ali", 15, Gender.None, "<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ClassToSerialize xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <Name>ali</Name>\r\n  <Age>15</Age>\r\n  <Gender>None</Gender>\r\n</ClassToSerialize>")]
+        [Theory]
+        public override async Task Serilize(string name, int age, Gender gender, string expected)
+        {
+            await base.Serilize(name, age, gender, expected);
+        }
+        [InlineData("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ClassToSerialize>\r\n  <Name>Mahdi</Name>\r\n  <Age>30</Age>\r\n  <Gender>Male</Gender>\r\n</ClassToSerialize>", "Mahdi", 30, Gender.Male)]
+        [InlineData("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ClassToSerialize xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <Name>Maryam</Name>\r\n  <Age>15</Age>\r\n  <Gender>Female</Gender>\r\n</ClassToSerialize>", "Maryam", 15, Gender.Female)]
+        [InlineData("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ClassToSerialize xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <Name>ali</Name>\r\n  <Age>15</Age>\r\n  <Gender>None</Gender>\r\n</ClassToSerialize>", "ali", 15, Gender.None)]
+        [Theory]
+        public override async Task Deserilize(string xml, string name, int age, Gender gender)
+        {
+            await base.Deserilize(xml, name, age, gender);
+        }
+    }
+}
