87ca0dca99e95c9e7e8d72bef28499cbf5dc503e
Saba Babaei <sabababaei1393@gmail.com>
2023-01-09 21:12:48 +0330

    Added Sample For MessagePack and BinaryGo

---
+++ Samples/CSharp/ASPCore/WebAPISampleProject/WebAPISampleProject/Models/User.cs
@@ -0,0 +1,15 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+using MessagePack;
+namespace WebAPISampleProject.Models;
+
+[MessagePackObject()]
+public class User
+{
+    [Key(0)]
+    public string FirstName { get; set; }
+    [Key(1)]
+    public string LastName { get; set; }
+    [Key(2)]
+    public int Age { get; set; }
+}
